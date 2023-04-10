9df0bbcd6db1cee04d9d928a10a9ac152cf355d0
Chuanwise <chuanwise@qq.com>
2023-04-10 17:59:40 +0800

    Add implementations of Expression and Interpreter based on JFlex and JavaCup

--- xiaoming-expression-api/src/main/java/cn/codethink/xiaoming/expression/Expression.java
+++ xiaoming-expression-api/src/main/java/cn/codethink/xiaoming/expression/Expression.java
@@ -1,9 +1,26 @@
 package cn.codethink.xiaoming.expression;
 
+import cn.codethink.xiaoming.expression.lang.Type;
+
 /**
  * <h1>表达式</h1>
  *
  * @author Chuanwise
  */
 public interface Expression {
+    
+    /**
+     * 计算表达式的值
+     *
+     * @return 表达式的值
+     * @throws CalculateException 计算过程中出现的异常
+     */
+    Object calculate() throws CalculateException;
+    
+    /**
+     * 获取表达式类型
+     *
+     * @return 表达式类型
+     */
+    Type getType();
 }
--- xiaoming-expression-api/src/main/java/cn/codethink/xiaoming/expression/api/API.java
+++ xiaoming-expression-api/src/main/java/cn/codethink/xiaoming/expression/api/API.java
@@ -1,5 +1,8 @@
 package cn.codethink.xiaoming.expression.api;
 
+import cn.codethink.xiaoming.expression.interpreter.CompileConfiguration;
+import cn.codethink.xiaoming.expression.interpreter.Interpreter;
+
 /**
  * <h1>表达式 API</h1>
  *
@@ -8,4 +11,10 @@ package cn.codethink.xiaoming.expression.api;
  * @author Chuanwise
  */
 public interface API {
+    
+    Interpreter getInterpreter();
+    
+    CompileConfiguration getCompileConfiguration();
+    
+    CompileConfiguration.Builder getCompileConfigurationBuilder();
 }
