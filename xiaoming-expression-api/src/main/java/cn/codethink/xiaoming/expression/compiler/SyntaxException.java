10a480cf310aa4386ff669a3e2f46f62a62e9810
Chuanwise <chuanwise@qq.com>
2023-04-10 13:23:12 +0800

    Add compile exceptions

---
+++ xiaoming-expression-api/src/main/java/cn/codethink/xiaoming/expression/compiler/SyntaxException.java
@@ -0,0 +1,25 @@
+package cn.codethink.xiaoming.expression.compiler;
+
+/**
+ * <h1>语法异常</h1>
+ *
+ * @author Chuanwise
+ */
+public class SyntaxException
+    extends CompileException {
+    
+    public SyntaxException() {
+    }
+    
+    public SyntaxException(String message) {
+        super(message);
+    }
+    
+    public SyntaxException(Throwable cause) {
+        super(cause);
+    }
+    
+    public SyntaxException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
