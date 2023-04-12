d34cdc5637555af1d9e2aa391221ee1f1430544f
Chuanwise <chuanwise@qq.com>
2023-04-12 18:11:09 +0800

    Recompile expression.cup and regenerate `parser` and `sym`

--- xiaoming-expression-core/src/main/java/cn/codethink/xiaoming/expression/acl/sym.java
+++ xiaoming-expression-core/src/main/java/cn/codethink/xiaoming/expression/acl/sym.java
@@ -6,40 +6,42 @@
 package cn.codethink.xiaoming.expression.acl;
 
 /** CUP generated class containing symbol constants. */
+@SuppressWarnings("all")
 public class sym {
-    /* terminals */
-    public static final int LBRACE = 4;
-    public static final int NULL_LITERAL = 15;
-    public static final int BOOLEAN_LITERAL = 12;
-    public static final int STRING_LITERAL = 14;
-    public static final int CHARACTER_LITERAL = 13;
-    public static final int EOF = 0;
-    public static final int FLOATING_POINT_LITERAL = 11;
-    public static final int RBRACK = 7;
-    public static final int COMMA = 8;
-    public static final int error = 1;
-    public static final int RPAREN = 3;
-    public static final int IDENTIFIER = 9;
-    public static final int LBRACK = 6;
-    public static final int LPAREN = 2;
-    public static final int RBRACE = 5;
-    public static final int INTEGER_LITERAL = 10;
-    public static final String[] terminalNames = new String[]{
-        "EOF",
-        "error",
-        "LPAREN",
-        "RPAREN",
-        "LBRACE",
-        "RBRACE",
-        "LBRACK",
-        "RBRACK",
-        "COMMA",
-        "IDENTIFIER",
-        "INTEGER_LITERAL",
-        "FLOATING_POINT_LITERAL",
-        "BOOLEAN_LITERAL",
-        "CHARACTER_LITERAL",
-        "STRING_LITERAL",
-        "NULL_LITERAL"
-    };
+  /* terminals */
+  public static final int LBRACE = 4;
+  public static final int NULL_LITERAL = 15;
+  public static final int BOOLEAN_LITERAL = 12;
+  public static final int STRING_LITERAL = 14;
+  public static final int CHARACTER_LITERAL = 13;
+  public static final int EOF = 0;
+  public static final int FLOATING_POINT_LITERAL = 11;
+  public static final int RBRACK = 7;
+  public static final int COMMA = 8;
+  public static final int error = 1;
+  public static final int RPAREN = 3;
+  public static final int IDENTIFIER = 9;
+  public static final int LBRACK = 6;
+  public static final int LPAREN = 2;
+  public static final int RBRACE = 5;
+  public static final int INTEGER_LITERAL = 10;
+  public static final String[] terminalNames = new String[] {
+  "EOF",
+  "error",
+  "LPAREN",
+  "RPAREN",
+  "LBRACE",
+  "RBRACE",
+  "LBRACK",
+  "RBRACK",
+  "COMMA",
+  "IDENTIFIER",
+  "INTEGER_LITERAL",
+  "FLOATING_POINT_LITERAL",
+  "BOOLEAN_LITERAL",
+  "CHARACTER_LITERAL",
+  "STRING_LITERAL",
+  "NULL_LITERAL"
+  };
 }
+
